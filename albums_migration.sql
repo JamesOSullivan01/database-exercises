d43ab8d353198138ed9480bb6380ed4c585e2e65
James OSullivan <james.a.osullivan01@gmail.com>
2023-07-11 12:20:44 -0500

    Completed the exercise for mySQL tables.

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50),
+                        name  VARCHAR(50) NOT NULL,
+                        release_date  INT NOT NULL,
+                        sales  FLOAT NOT NULL,
+                        genre VARCHAR(50) NULL,
+                        PRIMARY KEY (id)
+);
