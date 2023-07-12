8cd657c2dd4479867b3ea59de393bf0fb9ff8188
James OSullivan <james.a.osullivan01@gmail.com>
2023-07-12 16:44:09 -0500

    completed part two of 'WHERE' exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,6 +1,14 @@
 USE employees;
-    SELECT first_name FROM employees Where first_name IN ('Irena', 'Vidya', 'Maya');
+    SELECT first_name, gender FROM employees Where first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
+
+SELECT first_name, gender FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
 SELECT last_name FROM employees WHERE last_name LIKE 'E%';
 
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
 SELECT last_name FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
