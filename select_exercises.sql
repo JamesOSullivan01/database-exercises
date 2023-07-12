a925e1c39f4b7764d80e6de86504f0d84d7baa18
James OSullivan <james.a.osullivan01@gmail.com>
2023-07-12 14:13:15 -0500

    Completed walkthrough with Cody.

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,8 +1,8 @@
 USE codeup_test_db;
-    SELECT * FROM albums WHERE artist = 'Pinkfloyd';
-    SELECT * FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
-    SELECT * FROM albums WHERE  name = 'Thriller';
-    SELECT * FROM albums Where release_date BETWEEN 1990 AND 1999;
+    SELECT name FROM albums WHERE artist = 'Pinkfloyd';
+    SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+    SELECT genre FROM albums WHERE  name = 'Thriller';
+    SELECT name FROM albums Where release_date BETWEEN 1990 AND 1999;
     SELECT * FROM albums Where sales < 20;
     SELECT * FROM albums Where genre = 'rock';
 
