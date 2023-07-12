c7bf6ea96bbcbe189e43d50129760274cf236fd3
James OSullivan <james.a.osullivan01@gmail.com>
2023-07-12 10:44:50 -0500

    Finished queries for basic statements in SQL

---
+++ select_exercises.sql
@@ -0,0 +1,9 @@
+USE codeup_test_db;
+    SELECT * FROM albums WHERE artist = 'Pinkfloyd';
+    SELECT * FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+    SELECT * FROM albums WHERE  name = 'Thriller';
+    SELECT * FROM albums Where release_date BETWEEN 1990 AND 1999;
+    SELECT * FROM albums Where sales < 20;
+    SELECT * FROM albums Where genre = 'rock';
+
+
