e7f0f1148bbfd77aaf4abb4fb352d596d2b9ce03
James OSullivan <james.a.osullivan01@gmail.com>
2023-07-14 12:08:37 -0500

    Completed 'Group by' Exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,30 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT last_name FROM employees WHERE last_name
+LIKE 'E%' AND last_name LIKE '%E'
+GROUP BY last_name;
+
+SELECT DISTINCT last_name, first_name, COUNT(*) AS employee_count
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+GROUP BY last_name, first_name;
+
+SELECT DISTINCT last_name FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT count(last_name), last_name FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(gender), gender FROM employees
+Where first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
+
+
+
+
+
+
